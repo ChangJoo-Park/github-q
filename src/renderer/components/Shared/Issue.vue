55e73413ccffe481cfc909b663a8ed0c35233a77
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-09-30 22:15:02 +0900

    Using markdown-it to issue-body

--- src/renderer/components/Shared/Issue.vue
+++ src/renderer/components/Shared/Issue.vue
@@ -2,7 +2,7 @@
   <div v-if="issue">
     <h3><small>{{repository.full_name}}</small>&nbsp;{{ issue.title }}</h3>
     <issue-label v-for="label in labels" :key="label.id" :label="label" />
-    <div v-html="issue.body"></div>
+    <div class="issue-body" v-html="markdownParsedBody" />
     {{ createdInformation }} / {{ issue.comments }}
     <div class="">
       <issue-assignee v-for="assignee in assignees" :key="assignee.id" :assignee="assignee" />
@@ -11,9 +11,21 @@
 </template>
 
 <script>
+import MarkdownIt from 'markdown-it'
+
 import IssueLabel from '@/components/Shared/Issue/Label'
 import IssueUser from '@/components/Shared/Issue/User'
 import IssueAssignee from '@/components/Shared/Issue/Assignee'
+
+const md = MarkdownIt()
+  .use(require('markdown-it-checkbox'))
+  .use(require('markdown-it-link-attributes'), {
+    attrs: {
+      target: '_blank',
+      rel: 'noopener'
+    }
+  })
+
 export default {
   props: {
     issue: {
@@ -45,10 +57,18 @@ export default {
       const stateAt = state === 'open' ? this.issue.created_at : this.issue.closed_at
       const createdUser = this.user.login
       return `${issueNumber} ${pastState} at ${stateAt} by ${createdUser}`
+    },
+    markdownParsedBody () {
+      const body = this.issue.body || ''
+      return md.render(body)
     }
   }
 }
 </script>
 
 <style lang='css'>
+.issue-body img {
+  display: block;
+  max-height: 400px;
+}
 </style>
