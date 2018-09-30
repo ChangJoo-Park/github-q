e42ab4868b380c5c000db6ef234ad6c1cf649bba
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-09-30 21:57:49 +0900

    Using Issue component in Mentioned

--- src/renderer/components/AppPage/Mentioned.vue
+++ src/renderer/components/AppPage/Mentioned.vue
@@ -2,10 +2,8 @@
   <div>
     <h1>Mentioned Issues</h1>
     <loader-wrapper :is-loaded="isLoaded">
-      <div v-if="items">
-        <div v-for="item in items" :key="item.id">
-          <pre>{{ item }}</pre>
-        </div>
+      <div v-if="issues">
+        <issue v-for="issue in issues" :key="issue.id" :issue="issue"/>
       </div>
       <div v-else>
         <h1>There is no Assigned Issues</h1>
@@ -18,14 +16,16 @@
 import Service from '@/services'
 
 import LoaderWrapper from '@/components/Shared/LoaderWrapper'
+import Issue from '@/components/Shared/Issue'
 
 export default {
   components: {
-    LoaderWrapper
+    LoaderWrapper,
+    Issue
   },
   data () {
     return {
-      items: null,
+      issues: null,
       isLoaded: false
     }
   },
@@ -33,7 +33,7 @@ export default {
     setTimeout(() => {
       Service.getMentionedIssues().then(({ data }) => {
         this.isLoaded = true
-        this.items = data
+        this.issues = data
       })
     }, 100)
   }
