de6b74ce26bda23d51b51139ed67622378738aa3
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-09-30 18:17:20 +0900

    Add issu assignee component

---
+++ src/renderer/components/Shared/Issue/Assignee.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    {{ assignee }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    assignee: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
