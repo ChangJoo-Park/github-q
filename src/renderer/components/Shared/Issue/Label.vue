cd133b48a4ecb368c0a5883c5df1a8a3e5a0f06b
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-09-30 18:03:30 +0900

    Add issue label component

---
+++ src/renderer/components/Shared/Issue/Label.vue
@@ -0,0 +1,34 @@
+<template>
+  <span :style="labelStyle" class="label">
+    {{ label.name }}
+  </span>
+</template>
+
+<script>
+import isDarkColor from 'is-dark-color'
+
+export default {
+  props: {
+    label: {
+      type: Object
+    }
+  },
+  computed: {
+    labelStyle () {
+      const backgroundColor = this.label ? `#${this.label.color}` : `#000`
+      const color = isDarkColor(backgroundColor) ? '#fff' : `#000`
+      return {
+        backgroundColor, color
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.label {
+  box-sizing: border-box;
+  padding: 3px 6px;
+  border-radius: 3px;
+}
+</style>
