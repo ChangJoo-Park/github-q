b7943d506a1cc1f4e3f87e1d5ffd92629c6ffba5
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-01 00:07:43 +0900

    Add comment component

---
+++ src/renderer/components/Shared/Comment.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    {{ comment.body }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    comment: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
