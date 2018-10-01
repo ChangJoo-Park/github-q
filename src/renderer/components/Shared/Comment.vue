2c923c0a6ee3e5e3634999ce199b4e53ddc12183
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-01 22:06:43 +0900

    Add comments and comment

--- src/renderer/components/Shared/Comment.vue
+++ src/renderer/components/Shared/Comment.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="">
-    {{ comment.body }}
+  <div class="comment-wrapper">
+    <div class="comment-header" v-if="user">
+      <vs-avatar  size="50px" :src="user.avatar_url"/>
+    </div>
+    <div class="comment-info">
+      <strong v-if="user">{{ user.login }}</strong>
+      <span class="comment-date">{{ formattedUpdatedAt }}</span>
+      <div class="comment-body" v-html="comment.body">
+      </div>
+      <vs-divider />
+    </div>
   </div>
 </template>
 
 <script>
+import Utils from '@/utils'
+import { format } from 'date-fns'
+
 export default {
   props: {
     comment: {
       type: Object
+    }
+  },
+  computed: {
+    user () {
+      return this.comment.user || {}
+    },
+    parsedBody () {
+      const body = this.comment.body || ''
+      return Utils.parseMarkdown(body)
+    },
+    formattedUpdatedAt () {
+      return format(this.comment.updated_at, 'MMMM dd YYYY h:mm aa')
     }
   }
 }
 </script>
 
 <style lang="css">
+.comment-wrapper {
+  display: flex;
+}
+
+.comment-header {
+  margin-right: 1rem;
+  flex: 0;
+}
+
+.comment-date {
+  text-align: right;
+}
+
+.comment-info {
+  flex: 1;
+}
+
+.comment-body {
+  margin-top: 1rem;
+}
 </style>
---
+++ src/renderer/components/Shared/Comments.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <comment
+      v-for="comment in comments"
+      :key="comment.id"
+      :comment="comment"
+    />
+  </div>
+</template>
+
+<script>
+import Comment from '@/components/Shared/Comment'
+
+export default {
+  props: {
+    comments: {
+      type: Array,
+      default: () => []
+    }
+  },
+  components: {
+    Comment
+  }
+}
+</script>
+
+<style lang="css">
+</style>
