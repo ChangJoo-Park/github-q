a53d1175ae7e1667fc97195d6e237a50cb99942b
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-01 21:08:45 +0900

    Update scrollable wrapper

--- src/renderer/components/AppPage/Notifications.vue
+++ src/renderer/components/AppPage/Notifications.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Notifications</h1>
     <loader-wrapper :is-loaded="isLoaded">
-      <div v-if="items">
+      <div v-if="items" class="notification-wrapper">
         <notification v-for="item in items" :key="item.id" :notification="item"/>
       </div>
       <div v-else>
@@ -39,5 +39,8 @@ export default {
 </script>
 
 <style>
-
+.notification-wrapper {
+  overflow-y: auto;
+  height: calc(100vh - 40px);
+}
 </style>
