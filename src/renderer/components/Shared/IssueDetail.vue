5575ea482d7602dd95d106b119b220af670fd2b3
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-05 23:30:24 +0900

    Update issue details styles

--- src/renderer/components/Shared/IssueDetail.vue
+++ src/renderer/components/Shared/IssueDetail.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="issue-detail" v-if="issue">
     <div class="issue-detail-body">
-      <div class="issue-details-actions">
-        <vs-button vs-color="warning" vs-type="filled" @click="toggleBookmark">
-          <template v-if="isBookmarked">
-            Delete Bookmark
-          </template>
-          <template v-else>
-            Add Bookmark
-          </template>
-        </vs-button>&nbsp;
-        <slot />
-      </div>
       <div class="issue-detail-info">
-        <h3>{{ issue.title }}</h3>
+        <h1>{{ issue.title }}</h1>
       </div>
       <div v-html="parsedBody"></div>
       <vs-divider />
       <div class="issue-detail-comments">
+        <h4>Comments :</h4>
         <comments :comments="comments" />
       </div>
     </div>
+    <!-- Composer -->
     <div class="issue-detail-composer">
       <vs-textarea label="New Comment" v-model="newComment" />
       <vs-button vs-color="primary" vs-type="filled" @click.prevent="submitComment">Submit</vs-button>
+      <vs-button :vs-color="bookmarkedColor" vs-type="filled" @click="toggleBookmark">
+        <template v-if="isBookmarked">
+          Delete Bookmark
+        </template>
+        <template v-else>
+          Add Bookmark
+        </template>
+      </vs-button>&nbsp;
+      <slot />
     </div>
   </div>
   <div class="issue-detail-empty" v-else>
@@ -70,6 +70,9 @@ export default {
     ...mapGetters(['githubUser']),
     parsedBody () {
       return Util.parseMarkdown(this.issue.body)
+    },
+    bookmarkedColor () {
+      return this.isBookmarked ? 'warning' : 'success'
     }
   },
   mounted () {
@@ -134,8 +137,8 @@ export default {
 }
 
 .issue-detail {
-  overflow-y: auto;
-  height: calc(100vh - 40px);
+  overflow: hidden;
+  height: calc(100vh);
   display: flex;
   flex-direction: column;
   position: relative;
@@ -143,11 +146,13 @@ export default {
 
 .issue-detail-body {
   flex: 1;
+  overflow: auto;
 }
 
 .issue-detail-info {
   flex: 0;
   min-height: 20px;
+  margin-bottom: 1rem;
 }
 
 .issue-detail-comments {
@@ -155,15 +160,12 @@ export default {
 }
 
 .issue-detail-composer {
-  flex-shrink: 0;
-  min-height: 30px;
+  flex: 0;
+  min-height: 140px;
   padding: .5rem .5rem;
 }
 
 .issue-details-actions {
   text-align: right;
-  position: absolute;
-  top: 0;
-  right: 1rem;
 }
 </style>
