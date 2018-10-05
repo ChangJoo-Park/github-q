41a559ced9d07e886a8714eb3c7e7ac88ca6b1f3
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-05 22:41:10 +0900

    Using LoaderWrapper for bookmarked

--- src/renderer/components/AppPage/Bookmarked.vue
+++ src/renderer/components/AppPage/Bookmarked.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="bookmarked-wrapper">
-    <issue v-for="issue in issues" :key="issue.id" :issue="issue" />
-  </div>
+  <loader-wrapper :is-loaded="isLoaded">
+    <div v-if="issues" class="issue-wrapper">
+      <div class="issue-listing-wrapper">
+        <div class="issue-listings">
+          <issue
+          v-for="issue in issues"
+          :key="issue.id"
+          :issue="issue"
+          :is-selected="isSelectedIssue(issue)"
+          @click.native="onClickIssue(issue)"
+          />
+        </div>
+      </div>
+      <issue-detail class="issue-details" :issue="selectedIssue">
+        <vs-button vs-color="dark" vs-type="filled" @click="onCloseSelectedIssue">Close</vs-button>
+      </issue-detail>
+    </div>
+    <div v-else>
+      <h1>There is no Assigned Issues</h1>
+    </div>
+  </loader-wrapper>
 </template>
 
 <script>
@@ -10,15 +28,21 @@ import { mapGetters } from 'vuex'
 import Store from '@/store/db'
 import Service from '@/services'
 
+import LoaderWrapper from '@/components/Shared/LoaderWrapper'
 import Issue from '@/components/Shared/Issue'
+import IssueDetail from '@/components/Shared/IssueDetail'
 
 export default {
   components: {
-    Issue
+    LoaderWrapper,
+    Issue,
+    IssueDetail
   },
   data () {
     return {
-      issues: null
+      issues: null,
+      isLoaded: false,
+      selectedIssue: null
     }
   },
   computed: {
@@ -44,6 +68,20 @@ export default {
       this.issues = result.map((r) => {
         return r.data
       })
+      this.isLoaded = true
+    },
+    onClickIssue (issue) {
+      this.selectedIssue = issue
+    },
+    onCloseSelectedIssue () {
+      this.selectedIssue = null
+      this.fetchBookmarkedIssues()
+    },
+    isSelectedIssue (issue) {
+      if (!this.selectedIssue || !issue) {
+        return false
+      }
+      return issue.id === this.selectedIssue.id
     }
   }
 }
